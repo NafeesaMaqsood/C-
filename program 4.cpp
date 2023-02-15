b88eeb5d2a6eeeaf385351fd1896c895fbe0ffdc
NafeesaMaqsood <124290642+NafeesaMaqsood@users.noreply.github.com>
2023-02-15 10:38:53 +0500

    Add files via upload

---
+++ program 4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<ctype.h>
+using namespace std;
+int main()
+{
+cout<<"There was a men is peur\n";
+cout<<"Who dreamed the he was acting\n";
+cout<<"He work in light\n";
+cout<<"It was a lucky night\n";
+cout<<"He was a very happy\n";
+return 0;
+}
